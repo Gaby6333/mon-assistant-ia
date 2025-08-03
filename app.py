06043ba1e89df0545906a14606770701a247f93e
Gabriel <gabrieldurette17@gmail.com>
2025-08-03 19:58:35 -0400

    Ajout de l'app de résumé et du requirements

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from transformers import pipeline
+
+st.title("🧠 Résumeur de texte intelligent")
+
+texte = st.text_area("Colle ici ton texte à résumer 👇")
+
+if st.button("Résumer le texte"):
+    with st.spinner("Je réfléchis... 🤔"):
+        summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+        resultat = summarizer(texte, max_length=100, min_length=25, do_sample=False)
+        st.success("Résumé :")
+        st.write(resultat[0]['summary_text'])
